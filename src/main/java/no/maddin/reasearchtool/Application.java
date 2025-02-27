011a7e3f3056d248d01cacf5ebf5b2060bb247d4
Martin Goldhahn <mgoldhahn@gmail.com>
2025-02-27 23:17:25 +0100

    initial simple project setup

---
+++ src/main/java/no/maddin/reasearchtool/Application.java
@@ -0,0 +1,10 @@
+package no.maddin.reasearchtool;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}
